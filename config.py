97027ce2b0f361543c764bc1711c65361afeb31a
theoneandonly-amiko <ando1234gamer@gmail.com>
2024-06-17 08:24:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-TOKEN = 'MTI1MTU4NzM5NjgzMTU0MzMzNw.GOZCVo.B_BnR9L-7FQiomgz6BxmOJ-2TX2RI8x1jQ6g5g'
+TOKEN = 'YOUR_BOT_TOKEN_HERE' # Go to Discord Developer Portal and get one by create a bot (or an app)
 PREFIX = '?'
-YOUTUBE_API_KEY = "AIzaSyCxS7zfyZ2OxZDacg4IbXBo1PAdaWChggI"
-YOUTUBE_CHANNEL_ID = "UCDj4EDvpEy-O3PH54u_B3mg"
+YOUTUBE_API_KEY = "YOUR_YOUTUBE_API_KEY" # Go to Google CLoud Console and grab one.
+YOUTUBE_CHANNEL_ID = "YOUR_CHANNEL_ID" # You can add any channel you want the bot to keep tracking in RPC.
